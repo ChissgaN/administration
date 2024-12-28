f41d9ae6dc34a96229e02396dcbf7e0c2a0530ea
ChissgaN <coderchissgan@gmail.com>
2024-12-28 12:51:28 -0400

    Integrate sequelize and refactor StatusModel, StatusController and StatusRouter for sequelize integration

--- rp-api/src/models/StatusModel.js
+++ rp-api/src/models/StatusModel.js
@@ -1,60 +1,23 @@
-import { pool } from "../libs/db_conn.js";
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../libs/sequelize.js";
 
-export async function select(){
-    try{
-        const query = "SELECT * FROM status";
-        console.log(query);
-        const result = await pool.request().query(query);
-        return result.recordset;
-    } catch (error) {
-        throw error;
-    }
-}
+export class Status extends Model {}
 
-export async function find(status_id){
-    try{
-        const query = "SELECT * FROM status WHERE id = @status_id";
-        const request = await pool.request();
-        request.input("status_id", status_id);
-        const result = await request.query(query);
-        return result.recordset;
-    } catch (error) {
-        throw error;
-    }
-}
-
-export async function create(status){
-    try{
-        const request = await pool.request();
-        Object.entries(status).forEach(([key, value]) =>{
-            request.input(key, value);
-        });
-        await request.execute("sp_create_status");
-    } catch (error){
-        throw error;
-    }
-}
-
-export async function update(status){
-    try{
-        const request = await pool.request();
-        Object.entries(status).forEach(([key, value]) => {
-            request.input(key, value);
-        });
-        await request.execute("sp_update_status");
-    } catch (error) {
-        throw error;
-    }
-}
-
-export async function remove(status_id){
-    try{
-        const request = await pool.request();
-        request.input("status_id", status_id);
-        await request.execute("sp_update_status");
-    } catch (error) {
-        throw error;
-    }
-}
-
-export default { select, find, create, update, remove };
+Status.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "status",
+    timestamps: false,
+  }
+);
--- rp-api/src/routes/StatusRouter.js
+++ rp-api/src/routes/StatusRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { store, update, remove, index } from '../controllers/StatusController.js';
+import { store, update, remove, index, show } from '../controllers/StatusController.js';
 
 export const status_router = Router();
 
 status_router.get('/', index);
 status_router.post('/', store);
+status_router.get('/:id', show);
 status_router.put('/:id', update);
 status_router.delete('/:id', remove);
 
