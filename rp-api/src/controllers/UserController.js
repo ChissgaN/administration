1a503d1ab08637793f3fa9af4eab231e697de9be
ChissgaN <coderchissgan@gmail.com>
2024-12-28 13:44:07 -0400

    Refactor UserModel and UserController for sequelize integration

--- rp-api/src/controllers/UserController.js
+++ rp-api/src/controllers/UserController.js
@@ -1,10 +1,14 @@
-import UserModel from "../models/UserModel.js";
+import { User } from "../models/UserModel.js";
 import { storeSchema, updateSchema } from "../libs/joi/UsersSchema.js";
 import { hash } from "bcrypt";
 
 export async function index(req, res, next) {
   try {
-    const user = await UserModel.select();
+    const user = await User.findAll({
+      where: { status_id: 1 },
+      attributes: { exclude: ["password"] },
+      include: ["role", "status"],
+    });
     res.json(user);
   } catch (error) {
     next(error);
@@ -13,8 +17,11 @@ export async function index(req, res, next) {
 
 export async function show(req, res, next) {
   try {
-    const user = await UserModel.find(req.params.id);
-    if (!user) {
+    const user = await User.findByPk(req.params.id, {
+      attributes: { exclude: ["password"] },
+      include: ["role", "status"],
+    });
+    if (!user || user.status_id === 2) {
       throw { message: "User not found", status: 404 };
     }
     res.json(user);
@@ -28,8 +35,7 @@ export async function store(req, res, next) {
     await storeSchema.validateAsync(req.body);
     const hashedPassword = await hash(req.body.password, 10);
     req.body.password = hashedPassword;
-    console.log(hashedPassword);
-    await UserModel.create(req.body);
+    await User.create(req.body);
     res.json({ message: "User created successfully" });
   } catch (error) {
     next(error);
@@ -41,7 +47,7 @@ export async function update(req, res, next) {
     await updateSchema.validateAsync(req.body);
     const hashedPassword = await hash(req.body.password, 10);
     req.body.password = hashedPassword;
-    await UserModel.update({ ...req.body, user_id: req.params.id });
+    await User.update({ ...req.body, user_id: req.params.id });
     res.json({ message: "User updated successfully" });
   } catch (error) {
     next(error);
@@ -50,11 +56,11 @@ export async function update(req, res, next) {
 
 export async function remove(req, res, next) {
   try {
-    const user = await UserModel.find(req.params.id);
+    const user = await User.find(req.params.id);
     if (!user) {
       throw { message: "User not found", status: 404 };
     }
-    await UserModel.remove(2, req.params.id);
+    await User.remove(2, req.params.id);
     res.json({ message: "User removed successfully" });
   } catch (error) {
     next(error);
--- rp-api/src/models/UserModel.js
+++ rp-api/src/models/UserModel.js
@@ -1,62 +1,97 @@
-import { pool } from "../libs/db_conn.js";
-import { jsonParse } from "../helpers/JsonParse.js";
-export async function select() {
-  try {
-    const query = "SELECT * FROM vw_users for json path";
-    const result = await pool.request().query(query);
-    return jsonParse(result);
-  } catch (error) {
-    throw error;
+import { sequelize } from "../libs/sequelize.js";
+import { DataTypes, Model } from "sequelize";
+import { Status } from "./StatusModel.js";
+import { Role } from "./RoleModel.js";
+export class User extends Model {
+  static async create(user) {
+    try {
+      const request = await pool.request();
+      Object.entries(user).forEach(([key, value]) => {
+        request.input(key, value);
+      });
+
+      await request.execute("sp_register_user");
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async update(user) {
+    try {
+      const request = await pool.request();
+      Object.entries(user).forEach(([key, value]) => {
+        request.input(key, value);
+      });
+
+      await request.execute("sp_update_user");
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  static async remove(status_id, user_id) {
+    try {
+      const request = await pool.request();
+      request.input("status_id", status_id);
+      request.input("user_id", user_id);
+
+      await request.execute("sp_update_user_status");
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
-export async function find(user_id) {
-  try {
-    const query = `SELECT * FROM vw_users WHERE id = @user_id for json path, without_array_wrapper`;
-
-    const request = await pool.request();
-    request.input("user_id", user_id);
-    const result = await request.query(query);
-    return jsonParse(result);
-
-  } catch (error) {
-    throw error;
+User.init(
+  {
+    role_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "role",
+        key: "id",
+      },
+    },
+    status_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "status",
+        key: "id",
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birth_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "users",
+    timestamps: false,
   }
-}
+);
 
-export async function create(user) {
-  try {
-    const request = await pool.request();
-    Object.entries(user).forEach(([key, value]) => {
-      request.input(key, value);
-    });
-    await request.execute("sp_register_user");
-  } catch (error) {
-    throw error;
-  }
-}
+User.belongsTo(Status, {
+  foreignKey: "status_id",
+  as: "status",
+});
 
-export async function update(user) {
-  try {
-    const request = await pool.request();
-    Object.entries(user).forEach(([key, value]) => {
-      request.input(key, value);
-    });
-    await request.execute("sp_update_user");
-  } catch (error) {
-    throw error;
-  }
-}
+User.belongsTo(Role, {
+  foreignKey: "role_id",
+  as: "role",
+});
 
-export async function remove(status_id, user_id) {
-  try {
-    const request = await pool.request();
-    request.input("status_id", status_id);
-    request.input("user_id", user_id);
-    await request.execute("sp_update_user_status");
-  } catch (error) {
-    throw error;
-  }
-}
-
-export default { select, find, create, update, remove };
